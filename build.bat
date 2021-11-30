34d890ca6173bbcf3cb1a6cbe7b8f2e5d919fd60
Aiden <marcin.madry@interia.com>
2021-11-30 20:19:07 +0100

    Change building the project

--- build.bat
+++ build.bat
@@ -3,5 +3,6 @@ set CXX=g++
 
 set FLAGS=-Wall -Wextra -pedantic -std=c++17
 set FILES=main.cpp
+set LINK=-lwinmm -lpthread
 
-call %CXX% %FLAGS% -o windio-test %FILES% -lWinmm
+call %CXX% %FILES% %FLAGS% %LINK% -o windio-test
